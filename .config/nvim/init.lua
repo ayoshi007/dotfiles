ae06f9c87d4e62950ddc5a2a881fb68698ab86cf
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-03-23 15:51:33 -0500

    Append . prefix to files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- Configuration options are courtesy of Martin 'MartinLwx' Lin
+-- https://martinlwx.github.io/en/config-neovim-from-scratch/
+
+require('options')
+require('keymaps')
+require('plugins')
+require('colorscheme')
+require('lsp')
---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,30 @@
+-- define common options
+local opts = {
+    noremap = true, -- non-recursive
+    silent = true,  -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+-- See ':h vim.map.set()' for more information
+-- Window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Window resizing
+-- delta = 2 lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-----------------
+-- visual mode --
+-----------------
+-- starts visual mode with the same area as the previous area and the same mode
+--vim.keymap.set('v', '<', '<gv', opts)
+--vim.keymap.set('v', '>', '>gv', opts)
+
