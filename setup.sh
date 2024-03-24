c40287bf824148be94e2f6b8c0fb4aa5e1cf3574
akiyoshi <89048791+ayoshi007@users.noreply.github.com>
2024-03-24 14:05:35 -0500

    Run stow on each file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# run script as superuser
+apt install stow
+files="bash neovim vim tmux git"
+
+# back up and make stow symlinks for files
+for file in ${files}; do
+	echo "Creating stow symlink for ${file}..."
+	stow "$file" -t ~
+done
+
+
+echo "Getting plugins managers"
 # tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+echo "Installing tmux-plugin-manager (tpm)... "
+#git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+echo "done"
+
+
 
