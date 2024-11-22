82ee90063925132cf64cb9307ce4a9ea0829dcf1
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-11-22 17:01:17 -0600

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,97 +1,29 @@
 #!/bin/bash
 
-MYUSER="$SUDO_USER"
-MYHOME="/home/$MYUSER"
-SUDOME="sudo -u $MYUSER"
-
 echo "User ${MYUSER} running setup.sh as superuser"
+echo "Home directory ${MYHOME}"
+echo "Sudo me: ${SUDOME}"
+yes_or_none "Proceed?" || exit 1
 
-# run script as superuser
-deps="
-bash
-curl
-git
-stow
-"
-packages="
-bash
-bash-completion
-bear
-cmake
-curl
-fzf
-golang
-git
-htop
-jq
-lua5.4
-luajit
-luarocks
-nmap
-python3
-python3-pip
-ripgrep
-stow
-tmux
-tree
-zsh
-zsh-syntax-highlighting
-"
-stowlinks="
-bash
-neovim
-vim 
-tmux
-git
-"
+echo "Updating APT"
+apt update
 
 echo "Installing dependencies"
-apt update &&
-    apt install -y ${deps}
+apt install -y $(cat dependencies.txt)
 
-echo "Install packages"
-apt update &&
-    apt install -y ${packages}
+echo "Installing packages"
+apt install -y $(cat packages.txt)
 
-echo "Removing existing neovim installation in /opt/nvim-linux64/"
-rm -r /opt/nvim-linux64
-echo "Installing neovim into /opt/nvim-linux64/"
-$SUDOME curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-tar -xzf nvim-linux64.tar.gz
-sudo mv nvim-linux64 /opt
-rm nvim-linux64.tar.gz
+if yes_or_none "Stow adopt home config to ${MYHOME}?" ; then
+    stow --adopt home -t "${MYHOME}"
+    echo "Done"
+    yes_or_none "Restore source controlled configs?" && git restore . && echo "Done"
+fi
 
-echo "Creating stow symlinks"
-for link in ${stowlinks}; do
-	echo "Creating stow symlink for ${link}..."
-	stow "$link" -t $MYHOME
-done
-: '
-echo "Make zsh default shell"
-$SUDOME chsh -s $(which zsh)
-
-echo "Install oh-my-zsh"
-$SUDOME sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-'
-# echo "Installing rustup"
-# curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | $SUDOME sh -s -- --default-toolchain stable --profile default
-
-echo "Getting plugins managers"
-echo "Installing tmux-plugin-manager (tpm)... "
-git clone --depth=1 https://github.com/tmux-plugins/tpm $MYHOME/.config/tmux/plugins/tpm
-echo "done"
+yes_or_none "Install other applications?" && ./install_other.sh
 
 echo "Cleaning up with apt autoremove"
 apt autoremove -y
 
-function yes_or_none {
-    read -p "$* [y/(n)]: " yn
-    case $yn in
-	[yY]*) return 0 ;;
-    esac
-    return 1
-}
-
 yes_or_none "Reboot?" && reboot
-echo "Reboot machine to finish set up"
-
+echo "Reboot machine to finish"
