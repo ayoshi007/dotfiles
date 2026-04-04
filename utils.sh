2727c05cb3e9b25bc1930f9ddd5e6ef25971ceac
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2026-04-04 01:06:45 -0500

    Whitespace edits

--- utils.sh
+++ utils.sh
@@ -12,4 +12,4 @@ function yes_or_none {
 
 MYUSER="$USER"
 MYHOME="$HOME"
-SUDOME=$([ $UID == 0 ] && echo "" || echo "sudo -u $MYUSER" )
+SUDOME="$( [ $UID == 0 ] && echo "" || echo "sudo -u $MYUSER" )"
