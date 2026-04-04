e7155f8b015c0c80ef71ccfef99ffd57ae73847f
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2026-04-04 01:06:28 -0500

    Use root sudo for apt-get commands

--- setup.sh
+++ setup.sh
@@ -30,13 +30,13 @@ echo "Sudo me: ${SUDOME}"
 yes_or_none "Proceed?" ${_UTILS_SKIP_YESNO} || exit 1
 
 echo "Updating apt"
-$SUDOME apt-get update
+sudo apt-get update
 
 echo "Installing dependencies"
-$SUDOME apt-get install -y $(cat dependencies.txt)
+sudo apt-get install -y $(cat dependencies.txt)
 
 echo "Installing packages"
-$SUDOME apt-get install -y $(cat packages.txt)
+sudo apt-get install -y $(cat packages.txt)
 
 if yes_or_none "Stow adopt home config to ${MYHOME}?" ; then
     $SUDOME stow --adopt home -t "${MYHOME}"
@@ -47,7 +47,7 @@ fi
 yes_or_none "Install other applications?" && install_others
 
 echo "Cleaning up with apt autoremove"
-$SUDOME apt-get autoremove -y
+sudo apt-get autoremove -y
 
 yes_or_none "Reboot?" && reboot
 echo "Reboot machine to finish"
